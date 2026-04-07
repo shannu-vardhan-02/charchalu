15b4ba7bd20d6f074d914237669cab6f87446530
Shanmukh <shanmukhpedapatruni007@gmail.com>
2026-04-07 19:10:55 +0530

    feat : add login and logout controllers

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+// the above routes are all POST because they are changing the state of the server (creating a user, creating a session, deleting a session), if they were GET, they would be idempotent and not change the state of the server
 
 export default router;
