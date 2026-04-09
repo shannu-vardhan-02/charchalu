1e3adf25753c6a221c2797758fad33580370a670
Shanmukh <shanmukhpedapatruni007@gmail.com>
2026-04-10 02:15:16 +0530

    feat : add message model, routes, controllers

---
+++ server/src/controllers/message.controller.js
@@ -0,0 +1,110 @@
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+// fetch all users except the logged-in user to show as contacts in the chat sidebar
+// route : GET /api/messages/contacts
+export const getAllContacts = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    // fetch all users except the logged-in user
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password"); // exclude password field
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error fetching contacts:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// fetch all messages between the logged-in user and another user (identified by their ID in the route parameter)
+// route : GET /api/messages/:id
+export const getMessagesByUserId = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const { id: userToChatId } = req.params;
+
+    const messages = await Message.find({
+      $or: [
+        // i send u msg
+        { senderId: loggedInUserId, receiverId: userToChatId },
+        // u send me msg
+        { senderId: userToChatId, receiverId: loggedInUserId },
+      ],
+    }).sort({ createdAt: 1 }); // Sort messages by creation time (oldest first)
+    res.status(200).json(messages);
+    ``;
+  } catch (error) {
+    console.log("Error fetching messages:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// send a message from the logged-in user to another user (identified by their ID in the route parameter)
+// route : POST /api/messages/:id
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const senderId = req.user._id;
+    const { id: receiverId } = req.params;
+
+    let imageUrl;
+    if (image) {
+      // upload base5 image to cloudinary and get the URL
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    // create a new message document - this is the message model schema
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    // todo - send message in realtime if user is online using socket.io
+
+    // save the message to the database
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error sending message:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const getAllChats = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    // Fetch all messages where the logged-in user is either the sender or receiver
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+    }).sort({ createdAt: -1 }); // Sort messages by creation time (newest first)
+
+    // Extract unique user IDs of chat partners from the messages
+    // We do it by checking if the logged-in user is the sender or receiver and then taking the other ID as the chat partner
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUserId.toString()
+            ? msg.receiverId.toString()
+            : msg.senderId.toString(),
+        ),
+      ),
+    ];
+
+    // Fetch user details of the chat partners to display in the chat sidebar
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select("-password");
+
+    res.status(200).json(chatPartners);
+  } catch (error) {
+    console.log("Error fetching chats:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- server/src/routes/message.routes.js
+++ server/src/routes/message.routes.js
@@ -1,9 +1,23 @@
 import express from "express";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+  getAllChats,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Message sent successfully!");
-});
+// these middleware execute in order - so requests get rate-limited first, then authenticated
+// this is more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
+router.use(arcjetProtection, protectRoute);
+// Apply authentication middleware to all routes in this router because all routes require the user to be logged in, because we cannot give these msgs to everyone. Only logged in users can see their contacts and messages.
 
+router.get("/contacts", getAllContacts);
+router.get("/chats", getAllChats);
+router.get("/:id", getMessagesByUserId);
+
+router.post("/send/:id", sendMessage); // Send a message to a user by their ID
 export default router;
